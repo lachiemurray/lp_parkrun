6ab58b79e6b659cdbca20c78679564b864870fa6
Lachlan Murray <lachie.murray@gmail.com>
2013-01-17 23:15:46 +0000

    Transforming from hellow world to basic parkrun pub

--- urls.py
+++ urls.py
@@ -3,12 +3,12 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
 
-    url(r'^edition/$', 'lp_hello_django.views.edition'),
-    url(r'^sample/$', 'lp_hello_django.views.sample'),
-    url(r'^validate_config/$', 'lp_hello_django.views.validate_config'),
-    url(r'^configure/$', 'lp_hello_django.views.configure'),
-    url(r'^meta.json$', 'lp_hello_django.views.meta_json'),
-    url(r'^icon.png$', 'lp_hello_django.views.icon'),
+    url(r'^edition/$', 'lp_parkrun.views.edition'),
+    url(r'^sample/$', 'lp_parkrun.views.sample'),
+    url(r'^validate_config/$', 'lp_parkrun.views.validate_config'),
+    url(r'^configure/$', 'lp_parkrun.views.configure'),
+    url(r'^meta.json$', 'lp_parkrun.views.meta_json'),
+    url(r'^icon.png$', 'lp_parkrun.views.icon'),
     
 )
 
