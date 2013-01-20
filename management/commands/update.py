799effdb0a8d2011cd1f1fadd29775f9d49a313b
root <lachie.murray@gmail.com>
2013-01-20 21:36:21 +0000

    Skeleton of update cron

--- management/commands/update.py
+++ management/commands/update.py
@@ -1,10 +1,32 @@
 from django.core.management.base import BaseCommand, CommandError 
 from lp_parkrun.models import User, Event
 
+from lp_parkrun.tools import * 
+
 class Command(BaseCommand): 
-    args = '< >' 
+    
     help = 'Update local parkrun database'
 
     def handle(self, *args, **options): 
+
+        # Scrape for new parkruns 
+        self.stdout.write('Looking for new parkruns...\n')
     
-        self.stdout.write('Local parkrun database updated.\n')
+    
+    
+        events = ['mileend', 'york', 'bushy', 'harrogate']
+        for e in events:
+    	       if not Event.objects.filter(identifier=e).exists():
+                 AddEvent(e)
+    
+        # Update events
+        self.stdout.write('Updating parkruns...\n')
+        for e in Event.objects.all():
+            UpdateEvent(e)
+    
+        # Update users
+        self.stdout.write('Updating users...\n')
+        for u in User.objects.all():
+            UpdateUser(u)
+        
+        self.stdout.write('Done.\n')
