ec5c67de1b6b61f742744dc1fa2230bd1f45ae99
Lachlan Murray <lachie.murray@gmail.com>
2013-01-21 21:12:56 +0000

    changed how users and events displayed in admin interface

--- admin.py
+++ admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from lp_parkrun.models import User, Event
 
-admin.site.register(User)
-admin.site.register(Event)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('first_names', 'last_names', 'barcode')
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'identifier')
+
+
+admin.site.register(Event,EventAdmin)
+admin.site.register(User,UserAdmin)
+
