97ab8975c93f13084c768c45962fa9601da51b5f
Lachlan Murray <lachie.murray@gmail.com>
2013-01-23 22:33:23 +0000

    updated user data regexs

--- scraper.py
+++ scraper.py
@@ -9,14 +9,15 @@ class Scraper():
         self.user_agent = "Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11"
         
         # Compile regexs
+        self.unwanted_chars = re.compile("[aeiou-]")
+        
         self.event_re = re.compile("<h2>([a-zA-Z\s\.\-&;']*)parkrun.*#\s*([0-9]+)")
         self.postcode_re = re.compile('([A-PR-UWYZ0-9][A-HK-Y0-9][AEHMNPRTVXY0-9]?[ABEHMNPRVWXY0-9]? {1,2}[0-9][ABD-HJLN-UW-Z]{2}|GIR 0AA)')
         self.event_id_re = re.compile('<a href="http:\/\/www.parkrun.org.uk\/([a-zA-Z\-]*)\/results">')
         
-        self.user_pb = re.compile('<td>Time</td><td>([0-9]+:[0-9]+)<\/td>')
-        self.user_re = re.compile('<h2>(.*)</h2>',flags=re.DOTALL)
-        self.user_name_re = re.compile('(?=.*-)([A-Z]{1}[a-zA-Z\-]+)\s')
-        self.user_runs = re.compile('(?P<event_runs>[0-9]+).*at\s(?P<event>.*)<br\/>(?P<total_runs>[0-9]+)')
+        self.user_pb_re = re.compile('<td>Time</td><td>([0-9]+:[0-9]+)<\/td>')
+        self.user_name_re = re.compile("(?=.*-)([a-zA-Z]+[a-zA-Z\-']*)")
+        self.user_runs_re = re.compile('(?P<event_runs>[0-9]+).*at\s(?P<event>.*)<br\/>(?P<total_runs>[0-9]+)')
 
     # Scrape parkrun website and return a list of all parkrun events
     def scrape_event_ids(self):
@@ -55,16 +56,14 @@ class Scraper():
         
         page = f.read()
     
-        # Isolate the most important section of the page       
-        result = self.user_re.search(page)
+        # Isolate the most important section of the page    
+        result = page.split('h2')   
         
         if result:
-            if result.group(1):
-                user = result.group(1)               
+            user = result[1]
         
         
         ### Scrape names ###
-        
         result = self.user_name_re.findall(user)
         
         first_names = ''
@@ -83,8 +82,7 @@ class Scraper():
         
         
         ### Scrape runs ###
-        
-        result = self.user_runs.search(user)
+        result = self.user_runs_re.search(user)
                          
         if result:
             
@@ -101,7 +99,7 @@ class Scraper():
         
         
         ### Scrape PB ###
-        result = self.user_pb.search(page)
+        result = self.user_pb_re.search(page)
                 
         if result:
             
@@ -133,7 +131,6 @@ class Scraper():
             return event_data
         
         ### Scrape name and number ###
-        
         result = self.event_re.search(f.read())
       
         if result:
@@ -166,23 +163,21 @@ class Scraper():
     # Try to work out an event's twitter id   
     def get_twitter_id(self,identifier):
         print "Getting twitter id '%s'" % identifier
+    
+        username = identifier+'parkrun'
         
-        # TODO: check if suggested id is longer than
-        #     15 chars, and if so remove some vowels
-        
-        usernames = [identifier+'parkrun',identifier+'parkrunUK']
-
-        for u in usernames:
+        # Only check URL if username is a valid length
+        if len(username) <= 15:
             
-            request = urllib2.Request("http://www.twitter.com/"+u)
+            request = urllib2.Request("http://www.twitter.com/"+username)
             request.add_header("User-Agent",self.user_agent)
             
             try:
                 result = urllib2.urlopen(request)  
             except urllib2.HTTPError as detail:
                 print "Error opening URL %s: %s" % (request.get_full_url(), detail)
-                continue
+                return
             
             if result.getcode() == 200:
-                return u
+                return username
             
